d51c59a2d7d5e238faf2594c6deacadb5fde7518
Keerthan K <keerthank2005@gmail.com>
2025-07-04 16:23:13 +0530

    Remove Adjacent Duplicate Groups from Array

--- Stacks/Problems/Q4.java
+++ Stacks/Problems/Q4.java
@@ -7,26 +7,23 @@ package Problems;
 import java.util.Arrays;
 import java.util.Stack;
 
-
 public class Q4 {
     public static void main(String[] args) {
-        // int[] arr = {1,2,2,3,10,10,10,5,7,7,2};
-        int[] arr = {1, 5, 5, 5, 2};
+        // int[] arr = {1,2,2,3,10,10,10,5,7,7,2};  // [1, 3, 5, 2]
+        int[] arr = { 1, 5, 5, 5, 2 };      // [1, 2]
         int[] result = remove(arr);
-        System.out.println(Arrays.toString(result));        // [1, 3, 5, 2]
+        System.out.println(Arrays.toString(result)); 
     }
 
-
-    public static int[] remove(int[] arr){
+    public static int[] remove(int[] arr) {
         Stack<Integer> stack = new Stack<>();
 
-        for(int i=0 ;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             // If stack is empty or top is not equal to current
-            if(stack.isEmpty() || stack.peek()!=arr[i]){
+            if (stack.isEmpty() || stack.peek() != arr[i]) {
                 stack.push(arr[i]);
-            }
-            else{
-                if(i == arr.length-1 || arr[i]!=arr[i+1]){
+            } else {
+                if (i == arr.length - 1 || arr[i] != arr[i + 1]) {
                     stack.pop();
                 }
             }
@@ -34,7 +31,7 @@ public class Q4 {
 
         // Now transfer to array in correct order
         int[] result = new int[stack.size()];
-        for(int i=stack.size() - 1;i>=0;i--){
+        for (int i = stack.size() - 1; i >= 0; i--) {
             result[i] = stack.pop();
         }
         return result;
